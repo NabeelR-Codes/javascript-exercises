67633ca2166d953ea565a9ff9f7beac89cde3fec
Nabeel <258862149+NabeelR-Codes@users.noreply.github.com>
2026-05-28 14:53:04 -0400

    In the checker.js file I changed the operator from "===" to ">=" and changed 6 to 10, to suite what was needed.
    And in the .spec file I tested each test out by removing
    .skip, for each test one by one.

--- foundations/03_numberChecker/numberChecker.js
+++ foundations/03_numberChecker/numberChecker.js
@@ -1,5 +1,16 @@
+// function numberChecker(number) {
+//   if (number === 6) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
+
+// // Do not edit below this line
+// module.exports = numberChecker;
+
 function numberChecker(number) {
-  if (number === 6) {
+  if (number >= 10) {
     return true;
   } else {
     return false;
