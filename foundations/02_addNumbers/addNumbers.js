12c32ff2b6398d8863b25cd57d7f6a23531d0aa3
Nabeel <258862149+NabeelR-Codes@users.noreply.github.com>
2026-05-28 14:47:03 -0400

    Removed apostrophies from the letter on line 7 to fulfill the addition test.

--- foundations/02_addNumbers/addNumbers.js
+++ foundations/02_addNumbers/addNumbers.js
@@ -1,10 +1,25 @@
+// function addNumbers() {
+//   const a = 1;
+//   const b = 1;
+
+//   let result;
+
+//   result = "a" + "b"; // <------ EDIT THIS LINE
+
+//   return result;
+// }
+
+// // Do not change this
+// module.exports = addNumbers;
+
+
 function addNumbers() {
   const a = 1;
   const b = 1;
 
   let result;
 
-  result = "a" + "b"; // <------ EDIT THIS LINE
+  result = a + b; // <------ EDIT THIS LINE
 
   return result;
 }
