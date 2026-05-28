9de223957ab6296cd0cff9883b62a0bd14941ef5
Nabeel <258862149+NabeelR-Codes@users.noreply.github.com>
2026-05-28 14:43:36 -0400

    Added "helloWorld" to test out the .spec file.

--- foundations/01_helloWorld/helloWorld.js
+++ foundations/01_helloWorld/helloWorld.js
@@ -1,5 +1,11 @@
+// const helloWorld = function() {
+//   return ''
+// };
+
+// module.exports = helloWorld;
+
 const helloWorld = function() {
-  return ''
+  return 'Hello, World!'
 };
 
 module.exports = helloWorld;
